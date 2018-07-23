a2529b114fb18887c2451aa032a4e95341baf31a
Andrew Jones <andrewjones@Andrews-MacBook-Pro.local>
2018-07-23 18:33:35 +1000

    actually fix the sqrt thing

--- src/logKDE.cpp
+++ src/logKDE.cpp
@@ -17,10 +17,10 @@ using namespace Rcpp;
 
 // [[Rcpp::export]]
 double epanechnikov(double x){
-  double bound = std::sqrt(5);
-  double y = 3*(5-(x*x))/(20*std::sqrt(5))*(x>(-1*bound))*(x<(1*bound));
+  double bound = std::sqrt(5.0);
+  double y = 3*(5-(x*x))/(2*std::sqrt(5.0))*(x>(-1.0*bound))*(x<(1.0*bound));
   return y;
-}
+
 
 // [[Rcpp::export]]
 double gaussian(double x){
@@ -30,32 +30,32 @@ double gaussian(double x){
 
 // [[Rcpp::export]]
 double laplace(double x){
-  double val = 2/std::sqrt(2);
-  double y=val*std::exp(-1*(std::sqrt(2))*std::abs(x));
+  double val = 2.0/std::sqrt(2.0);
+  double y=val*std::exp(-1.0*(std::sqrt(2.0))*std::abs(x));
   return y;
 }
 
 // [[Rcpp::export]]
 double logistic(double x){
-  double bound = std::sqrt(3);
-  double tmp = std::pow(std::cosh(M_PI*x/(2*bound)),-2.0);
-  double y=(M_PI/(4*bound))*tmp;
+  double bound = std::sqrt(3.0);
+  double tmp = std::pow(std::cosh(M_PI*x/(2.0*bound)),-2.0);
+  double y=(M_PI/(4.0*bound))*tmp;
   return y;
 }
 
 // [[Rcpp::export]]
 double triangular(double x){
-  double bound = std::sqrt(6);
-  //double y = ((bound/6.0) -std::abs(x))*(x>(-1*bound))*(x<(1*bound));
-  double y = ((1 - std::abs(x)/bound)/bound)*(std::abs(x)< bound);
+  double bound = std::sqrt(6.0);
+  //double y = ((bound/6.0) -std::abs(x))*(x>(-1.0*bound))*(x<(1.0*bound));
+  double y = ((1.0 - std::abs(x)/bound)/bound)*(std::abs(x)< bound);
   return y;
 }
 
 
 // [[Rcpp::export]]
 double uniform(double x){
-  double bound = std::sqrt(3);
-  double y=(1/(2*bound))*(x>(-1*bound))*(x<(1*bound));
+  double bound = std::sqrt(3.0);
+  double y=(1.0/(2.0*bound))*(x>(-1.0*bound))*(x<(1.0*bound));
 
   return y;
 }
@@ -66,7 +66,7 @@ double KDE_e(double x, std::vector<double> xi, double h){
   int n = xi.size();
   std::vector<double> tmp(n);
   std::transform(xi.begin(), xi.end(), tmp.begin(), [x,h](double xi){return epanechnikov((x-xi)/h);});
-  double dens_x = (1/(n*h))*std::accumulate(tmp.begin(), tmp.end(), 0.0);
+  double dens_x = (1.0/(n*h))*std::accumulate(tmp.begin(), tmp.end(), 0.0);
   return(dens_x);
 }
 
@@ -75,7 +75,7 @@ double KDE_g(double x, std::vector<double> xi, double h){
   int n = xi.size();
   std::vector<double> tmp(n);
   std::transform(xi.begin(), xi.end(), tmp.begin(), [x,h](double xi){return gaussian((x-xi)/h);});
-  double dens_x = (1/(n*h))*std::accumulate(tmp.begin(), tmp.end(), 0.0);
+  double dens_x = (1.0/(n*h))*std::accumulate(tmp.begin(), tmp.end(), 0.0);
   return(dens_x);
 }
 // [[Rcpp::export]]
@@ -83,7 +83,7 @@ double KDE_lo(double x, std::vector<double> xi, double h){
   int n = xi.size();
   std::vector<double> tmp(n);
   std::transform(xi.begin(), xi.end(), tmp.begin(), [x,h](double xi){return logistic((x-xi)/h);});
-  double dens_x = (1/(n*h))*std::accumulate(tmp.begin(), tmp.end(), 0.0);
+  double dens_x = (1.0/(n*h))*std::accumulate(tmp.begin(), tmp.end(), 0.0);
   return(dens_x);
 }
 // [[Rcpp::export]]
@@ -91,7 +91,7 @@ double KDE_t(double x, std::vector<double> xi, double h){
   int n = xi.size();
   std::vector<double> tmp(n);
   std::transform(xi.begin(), xi.end(), tmp.begin(), [x,h](double xi){return triangular((x-xi)/h);});
-  double dens_x = (1/(n*h))*std::accumulate(tmp.begin(), tmp.end(), 0.0);
+  double dens_x = (1.0/(n*h))*std::accumulate(tmp.begin(), tmp.end(), 0.0);
   return(dens_x);
 }
 // [[Rcpp::export]]
@@ -99,7 +99,7 @@ double KDE_la(double x, std::vector<double> xi, double h){
   int n = xi.size();
   std::vector<double> tmp(n);
   std::transform(xi.begin(), xi.end(), tmp.begin(), [x,h](double xi){return laplace((x-xi)/h);});
-  double dens_x = (1/(2*n*h))*std::accumulate(tmp.begin(), tmp.end(), 0.0);
+  double dens_x = (1.0/(2.0*n*h))*std::accumulate(tmp.begin(), tmp.end(), 0.0);
   return(dens_x);
 }
 // [[Rcpp::export]]
@@ -107,7 +107,7 @@ double KDE_u(double x, std::vector<double> xi, double h){
   int n = xi.size();
   std::vector<double> tmp(n);
   std::transform(xi.begin(), xi.end(), tmp.begin(), [x,h](double xi){return uniform((x-xi)/h);});
-  double dens_x = (1/(n*h))*std::accumulate(tmp.begin(), tmp.end(), 0.0);
+  double dens_x = (1.0/(n*h))*std::accumulate(tmp.begin(), tmp.end(), 0.0);
   return(dens_x);
 }
 
@@ -116,8 +116,8 @@ double KDE_u(double x, std::vector<double> xi, double h){
 //// [[Rcpp::export]]
 // double silverman(std::vector<double> x){
 //   accumulator_set<double, features<tag::variance(lazy)>> acc;
-//   for_each(x.begin(), x.end(), boost::bind<void> (boost::ref(acc), _1));
-//   return 1.06*std::sqrt(variance(acc))*std::pow((x.size()),-0.2);
+//   for_each(x.begin(), x.end(), boost::bind<void> (boost::ref(acc), _1.0));
+//   return 1.06*std::sqrt((double)variance(acc))*std::pow((x.size()),-0.2);
 //
 // }
 
@@ -162,7 +162,7 @@ std::vector<double> logKDE(const std::vector<double>& input, const std::vector<d
 
 
 
-  std::transform( ret.begin(), ret.end(),support.begin(), ret2.begin(), [](double d, double x) {return (1/x)*d;} );
+  std::transform( ret.begin(), ret.end(),support.begin(), ret2.begin(), [](double d, double x) {return (1.0/x)*d;} );
   return ret2;
 }
 
